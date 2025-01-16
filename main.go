1003fe16073425c755ec257c74984d1ebd5ac037
Jonas Krogh <xyzkrgu@gmail.com>
2025-01-16 20:45:38 +0100

    Add healthz endpoint

--- main.go
+++ main.go
@@ -8,11 +8,18 @@ import (
 func main() {
 	sm := http.NewServeMux()
 
-	sm.Handle("/", http.FileServer(http.Dir('.')))
+	sm.HandleFunc("/healthz", healthHandler)
+	sm.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir('.'))))
 
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: sm,
 	}
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte("OK"))
 }
